cf16df250c865e5af75de39a0f82f676f97ce1ff
AtoCrafter <ato.crafter@gmail.com>
2015-01-09 15:13:14 +0900

    Add Recipe

--- src/main/scala/ato/tempblocks/TempBlocks.scala
+++ src/main/scala/ato/tempblocks/TempBlocks.scala
@@ -7,6 +7,8 @@ import cpw.mods.fml.common.Mod.EventHandler
 import cpw.mods.fml.common.event.FMLPreInitializationEvent
 import cpw.mods.fml.common.registry.GameRegistry
 import cpw.mods.fml.common.{Mod, SidedProxy}
+import net.minecraft.init.{Blocks, Items}
+import net.minecraft.item.ItemStack
 
 @Mod(modid = "TempBlocks", modLanguage = "scala")
 object TempBlocks {
@@ -28,5 +30,24 @@ object TempBlocks {
     GameRegistry.registerBlock(blockChain, "ChainBlock")
 
     GameRegistry.registerTileEntity(classOf[TileEntityCollapse], "CollapseBlock")
+
+    GameRegistry.addRecipe(new ItemStack(blockFrame, 64),
+      "SSS",
+      "W W",
+      "SSS",
+      Character.valueOf('S'), Items.stick,
+      Character.valueOf('W'), Blocks.planks)
+    GameRegistry.addRecipe(new ItemStack(blockCollapse, 64),
+      "GGG",
+      "GFG",
+      "GGG",
+      Character.valueOf('G'), Blocks.glass,
+      Character.valueOf('F'), Items.flint)
+    GameRegistry.addRecipe(new ItemStack(blockChain, 64),
+      "FFF",
+      "S S",
+      "FFF",
+      Character.valueOf('F'), Blocks.iron_bars,
+      Character.valueOf('S'), Items.string)
   }
 }
